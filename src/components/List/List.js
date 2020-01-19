b80484991e210d342a6fd90cbe4ac3cc90bb096f
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-01-19 15:12:08 +0100

    Add praser function

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
@@ -24,7 +25,7 @@ class List extends React.Component {
         <section className={styles.component}>
           <Hero titleText={this.props.title} image={this.props.image} />
           <div className={styles.description}>
-            {this.props.description}
+            {ReactHtmlParser(this.props.description)}
           </div>
           <div className={styles.columns}>
             <Column titleColumn={'Animals'} />
