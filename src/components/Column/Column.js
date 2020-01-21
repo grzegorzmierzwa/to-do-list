15512e5f65819c54f90bba6f6ad8d014362a94f9
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-01-21 01:39:03 +0100

    add icons

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import { settings } from '../../data/dataStore';
+import {settings, listData } from '../../data/dataStore';
 import Creator from '../Creator/Creator.js';
+import Icon from '../Icons/Icon.js';
 
 class Column extends React.Component {
   state = {
@@ -32,7 +33,7 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon}/></span>{this.props.title}</h3>
         <div>
           {this.state.cards.map(({key, ...cardProps}) => (
             <Card  key={key} {...cardProps} />
