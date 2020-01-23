b9fda718d1095e9d19259ce844ad971623e8502f
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-01-23 21:58:27 +0100

    Add propTypes

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -14,6 +14,7 @@ class Column extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
     cards: PropTypes.array,
+    icon: PropTypes.node,
   }
 
   addCard(title){
