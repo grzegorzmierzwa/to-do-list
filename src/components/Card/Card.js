4ef5aa13d28a910da7c08a5302eac6a19454565f
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-01-29 00:27:06 +0100

    Add creator in redux store

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
 
 class Card extends React.Component {
   static propTypes = {
     title: PropTypes.node,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultCardIcon,
   }
 
   render() {
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icons/Icon.js';
 import {settings} from '../../data/dataStore';
 
@@ -13,6 +13,7 @@ class Column extends React.Component {
     title: PropTypes.node.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
@@ -20,7 +21,7 @@ class Column extends React.Component {
   }
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}><Icon name={icon}/></span>{title}</h3>
@@ -29,9 +30,9 @@ class Column extends React.Component {
             <Card  key={key} {...cardProps} />
           ))}
         </div>
-        {/* <div>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+        <div>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
       </section>
     );
   }
