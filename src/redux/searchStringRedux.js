87f41d78d4e77f35815b075d25c9d552d034b157
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-02-03 00:08:55 +0100

    Add search string

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -4,14 +4,20 @@ export const countVisibleCards = ({cards, searchString}) => cards.filter(card =>
 export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const SEARCH = createActionName('SEARCH');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload, type: SEARCH });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case SEARCH:
+      return action.payload;
     default:
       return statePart;
   }
