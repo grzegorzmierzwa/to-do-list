7a9fc8c0b2e03b2da58e771006de4f050c195cec
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-02-09 20:34:42 +0100

    Add Search Result Container

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,32 +2,28 @@ import React from 'react';
 import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Icon from '../Icons/Icon.js';
-import {settings} from '../../data/dataStore';
-import {Link} from 'react-router-dom';
+import Container from '../Container/Container';
+// import Icon from '../Icons/Icon.js';
+// import {settings} from '../../data/dataStore';
+// import {Link} from 'react-router-dom';
 
-const SearchResults = ({id, title, icon, cards}) => (
-  <Link to={`/search/${id}`} className={styles.link}>
+const SearchResults = ({cards}) => (
+  // <Link to={`/${id}`} className={styles.link}>
+  <Container>
     <section className={styles.component}>
-      <h3 className={styles.title}><span className={styles.icon}><Icon name={icon}/></span>{title}</h3>
       <div>
         {cards.map(({id, ...cardProps}) => (
           <Card  key={id} {...cardProps} />
         ))}
       </div>
     </section>
-  </Link>
+  </Container>
+  // </Link>
 );
 
 SearchResults.propTypes = {
-  id: PropTypes.string,
-  title: PropTypes.node.isRequired,
+  // id: PropTypes.string,
   cards: PropTypes.array,
-  icon: PropTypes.node,
-};
-
-SearchResults.defaultProps = {
-  icon: settings.defaultColumnIcon,
 };
 
 export default SearchResults;
